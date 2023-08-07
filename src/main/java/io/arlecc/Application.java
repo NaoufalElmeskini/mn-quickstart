a7039135d603092f38ec08d2a2efeb99e985e0e0
Naoufal <NaoufalElmeskini>
2023-08-07 07:14:22 +0000

    Initial commit

---
+++ src/main/java/io/arlecc/Application.java
@@ -0,0 +1,10 @@
+package io.arlecc;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
