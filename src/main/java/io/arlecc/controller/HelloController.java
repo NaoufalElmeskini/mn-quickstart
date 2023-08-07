f67f48cd51e4591341e98390f9d00474d2957e5c
Naoufal EL MESKINI <naoufal_el_meskini@carrefour.com>
2023-08-07 16:29:56 +0200

    using configuration value

--- src/main/java/io/arlecc/controller/HelloController.java
+++ src/main/java/io/arlecc/controller/HelloController.java
@@ -1,6 +1,8 @@
 package io.arlecc.controller;
 
 import io.arlecc.service.HelloService;
+import io.micronaut.context.annotation.Property;
+import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import org.slf4j.Logger;
@@ -11,15 +13,22 @@ public class HelloController {
 
     private final HelloService helloService;
     private static final Logger log = LoggerFactory.getLogger(HelloController.class);
+    private final String language;
 
-    public HelloController(HelloService helloService) {
+    public HelloController(HelloService helloService,
+                           @Property(name= "hello.language") String language) {
         this.helloService = helloService;
+        this.language = language;
     }
-
 
     @Get
     public String hello() {
         log.debug("calling hello...");
         return helloService.helloFromService();
+    }
+
+    @Get(uri="/language", produces = MediaType.TEXT_PLAIN)
+    public String translation() {
+        return language;
     }
 }
